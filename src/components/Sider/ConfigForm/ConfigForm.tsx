7b542017c815cd46af2837a983c4a16ed14f01c5
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-03-05 08:25:17 +0200

    implementing_visualize_&_speed_functionality_controls

--- src/components/Sider/ConfigForm/ConfigForm.tsx
+++ src/components/Sider/ConfigForm/ConfigForm.tsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
 import { Button, Flex, Select, Slider, Typography } from "antd";
 import { EyeOutlined, ReloadOutlined } from "@ant-design/icons";
+import configsContext from "src/contexts/configsContext";
 import algoOptions from "./data";
+import { actions } from "src/types/Context";
 import Style from "./ConfigForm.module.css";
 
 export default (): JSX.Element => {
   const speedFormatter = (value?: number) => `${value} ms`;
   const lengthFormatter = (value?: number) => `${value! * 10}`;
+
+  const {
+    state: { isSorting },
+    dispatch,
+  } = useContext(configsContext);
 
   return (
     <form className={Style.form}>
@@ -16,11 +24,19 @@ export default (): JSX.Element => {
           style={{ width: "100%" }}
           options={algoOptions}
           defaultActiveFirstOption
+          disabled={isSorting}
         />
         <Flex>
           <Slider
             className={Style.slider}
+            onChange={(value) =>
+              dispatch({ type: actions.SET_SPEED, payload: value })
+            }
             tooltip={{ formatter: speedFormatter }}
+            disabled={isSorting}
+            min={5}
+            max={100}
+            step={5}
           />
           <Typography.Text className={Style.label}>Speed</Typography.Text>
         </Flex>
@@ -29,14 +45,25 @@ export default (): JSX.Element => {
             className={Style.slider}
             tooltip={{ formatter: lengthFormatter }}
             min={0.5}
+            disabled={isSorting}
           />
           <Typography.Text className={Style.label}>Length</Typography.Text>
         </Flex>
         <Flex className={Style.submit}>
-          <Button type="primary" icon={<EyeOutlined />}>
+          <Button
+            type="primary"
+            icon={<EyeOutlined />}
+            onClick={() => {
+              dispatch({ type: actions.SET_IS_SORTING, payload: true });
+            }}
+          >
             Visualize
           </Button>
-          <Button type="default" icon={<ReloadOutlined />} />
+          <Button
+            type="default"
+            icon={<ReloadOutlined />}
+            disabled={isSorting}
+          />
         </Flex>
       </Flex>
     </form>
--- src/hooks/useBubbleSort.ts
+++ src/hooks/useBubbleSort.ts
@@ -5,7 +5,7 @@ import { actions } from "src/types/Context";
 
 export default () => {
   const {
-    state: { list, speed },
+    state: { list, speed, isSorting, isSorted },
     dispatch,
   } = useContext(configsContext);
 
@@ -13,25 +13,26 @@ export default () => {
     const timers: NodeJS.Timeout[] = [];
     for (let i = 0; i < list.length; i++) {
       for (let j = 0; j < list.length - i - 1; j++) {
-        timers.push(
-          setTimeout(() => {
-            dispatch({ type: actions.SET_SELECTED, payload: [j, j + 1] });
-            dispatch({ type: actions.SET_IS_SORTING, payload: true });
-            if (list[j] > list[j + 1])
-              dispatch({
-                type: actions.SET_LIST,
-                payload: swap(j, j + 1, list),
-              });
-            if (i === list.length - 2) {
-              dispatch({ type: actions.SET_IS_SORTING, payload: false });
-              dispatch({ type: actions.SET_IS_SORTED, payload: true });
-            }
-          }, speed * i * list.length + j * speed)
-        );
+        if (isSorting && !isSorted)
+          timers.push(
+            setTimeout(() => {
+              dispatch({ type: actions.SET_SELECTED, payload: [j, j + 1] });
+              dispatch({ type: actions.SET_IS_SORTING, payload: true });
+              if (list[j] > list[j + 1])
+                dispatch({
+                  type: actions.SET_LIST,
+                  payload: swap(j, j + 1, list),
+                });
+              if (i === list.length - 2) {
+                dispatch({ type: actions.SET_IS_SORTING, payload: false });
+                dispatch({ type: actions.SET_IS_SORTED, payload: true });
+              }
+            }, speed * i * list.length + j * speed)
+          );
       }
     }
     return () => {
       timers.forEach((timer) => clearTimeout(timer));
     };
-  }, []);
+  }, [isSorting, isSorted, speed]);
 };
